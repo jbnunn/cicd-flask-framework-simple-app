c7cab91ddb20bcf11e18cad3310d7abf6741c28c
Jeff Nunn <jbnunn@gmail.com>
2017-10-18 18:07:16 -0500

    Adding POST to the index

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
     return jsonify({
-        'hello': 'world'
+        'message': 'pong'
     })
 
 if __name__ == '__main__':
